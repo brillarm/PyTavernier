6bf5706240b18e99b6fd350ffbfe5ff327ae25c2
ippei <mb.mathieubrillard@gmail.com>
2025-03-28 19:46:59 +0100

    Improve main file

--- src/index.py
+++ src/index.py
@@ -1,11 +1,24 @@
-import asyncio
+from asyncio import run
+from signal import SIGTERM, signal
 
-from lib.logger import DiscordLoguruHandler, logger
+from discord.client import (
+    ConnectionClosed,
+    GatewayNotFound,
+    HTTPException,
+    LoginFailure,
+)
+
+from lib.logger import logger
 from lib.settings import SETTINGS
 from models.bot import Tavernier
 
 
-def main():
+def signal_handler(sig, frame):
+    logger.success("Client have gracefully shut down (SystemExit).")
+    exit(0)
+
+
+async def main():
     intents = Tavernier.get_needed_intents()
     tavernier = Tavernier(
         SETTINGS.PREFIX,
@@ -13,16 +26,31 @@ def main():
         application_id=SETTINGS.CLIENT_ID,
         intents=intents,
     )
-    asyncio.run(tavernier.up_and_running())
-    try:
-        tavernier.run(
-            token=SETTINGS.TOKEN, log_handler=DiscordLoguruHandler(), log_level=1
-        )
-    except Exception:
-        logger.exception("Client encountered an unexpected exception:")
-    else:
-        logger.success("Client have gracefully shut down.")
+    async with tavernier:
+        await tavernier.up_and_running()
+        try:
+            await tavernier.login(token=SETTINGS.TOKEN)
+        except LoginFailure:
+            logger.exception("The used token is not valid.")
+        except HTTPException:
+            logger.exception("And unknown HTTP error has occurred.")
+
+        try:
+            await tavernier.connect(reconnect=True)
+        except GatewayNotFound:
+            logger.exception(
+                "The connection cannot be established with discord WebSocket, gateway was not found."
+            )
+        except ConnectionClosed:
+            logger.exception("The connection to discord WebSocket has been terminated.")
 
 
 if __name__ == "__main__":
-    main()
+    # handle docker signals properly
+    signal(SIGTERM, signal_handler)
+    try:
+        run(main())
+    except Exception:
+        logger.exception("Client encountered an unexpected exception:")
+    except KeyboardInterrupt:
+        logger.success("Client have gracefully shut down.")
